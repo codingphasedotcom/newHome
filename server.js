a0bc6b837a443fac8489829196ae1198122c9f7d
codingphasedotcom <codingphase@gmail.com>
2018-02-09 11:27:24 -0500

    Revert "Added Favicon"

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
-/* Core Modules */
-const path = require('path')
-/* Installed Modules*/
 const express = require('express')
 const serveStatic = require('serve-static')
-const favicon = require('serve-favicon');
-
+const path = require('path')
 // create the express app
-const app = express();
-
+const app = express()
 // create middleware to handle the serving the app
 app.use("/", serveStatic ( path.join (__dirname, '/public') ) )
-// Serves favicon
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
 // Catch all routes and redirect to the index file
 app.get('*', function (req, res) {
     res.sendFile(__dirname + '/public/index.html')
 })
-
 // Create default port to serve the app on
 const port = process.env.PORT || 5000
 app.listen(port)
